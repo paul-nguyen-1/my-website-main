33db06d90a9ec63078a14d75e60cec9a72690829
paul-nguyen96 <paul121596@icloud.com>
2023-10-22 23:50:15 -0500

    commit

--- components/Home/Maintenance/Maintenance.tsx
+++ components/Home/Maintenance/Maintenance.tsx
@@ -25,9 +25,9 @@ export default function Maintenance() {
             No problem contact me on{" "}
             <a
               className="text-white underline"
-              href="mailto:abdellatif@anaflous.com?subject=Hello%20World&body=Body%20of%20the%20email"
+              href="mailto:paul121596@icloud.com?subject=Hello%20World&body=Body%20of%20the%20email"
             >
-              abdellatif@anaflous.com
+              paul121596@icloud.com
             </a>
           </p>
         </div>
